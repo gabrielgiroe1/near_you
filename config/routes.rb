c30f6ac5e7e3db7b17acc4d71d66fd57c4db02dc
gabrielgiroe1 <gabriel.giroe@gmail.com>
2025-05-23 12:40:27 +0300

    Add solid queue, solid cache and solid cable

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  mount MissionControl::Jobs::Engine, at: "/jobs"
+
   get "up" => "rails/health#show", :as => :rails_health_check
+
   authenticate :user, lambda { |u| u.admin? } do
     mount Avo::Engine, at: Avo.configuration.root_path
   end
