e47510da66717017879b0411ca36bf316edd6d0c
Gabi <83423182+gabrielgiroe1@users.noreply.github.com>
2025-06-21 11:33:01 +0300

    Add initial RSpec model specs and factories

---
+++ spec/factories/appointments.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :appointment do
+    association :user
+    association :provider
+    start_time { Time.zone.parse('2023-01-01 10:00') }
+    status { :pending }
+  end
+end
